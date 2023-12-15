d469e28a08dace9ee9982401e6a4c8feae03fc3b
unknown <kidus.muluwork@gmail.com>
2023-12-15 19:07:34 +0300

    initalized and setup all the file and folder structures

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Nav, Header } from "./components/ui/index";
+
+function App() {
+  return (
+    <>
+      {/* {isSignedIn? <} */}
+      <Header />
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Nav.jsx
@@ -0,0 +1,5 @@
+function Nav() {
+  return <div className="bg-slate-600">Nav</div>;
+}
+
+export default Nav;
